19f9bd79812371eeaf8856fcbde350a8074bd276
Saurabh2322 <saurabhsingh2322@gmail.com>
2025-05-06 04:16:54 +0530

    changes

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.missingperson',
+    'core.core.missingperson',
 
 ]
 EXTERNAL_APPS = ['missingperson']
