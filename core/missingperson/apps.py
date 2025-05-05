fc8b81142b5573bfa419d32408ab159d82e74e86
Saurabh2322 <saurabhsingh2322@gmail.com>
2025-05-06 04:12:47 +0530

    changes

--- core/missingperson/apps.py
+++ core/missingperson/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class MissingpersonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'missingperson'
+    name = 'core.missingperson'
 
 
